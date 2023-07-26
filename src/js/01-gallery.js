e6587169194e80091203c2b94deb40360fc0acdd
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-07-26 14:58:45 +0300

    1 - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import simpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerGallery = document.querySelector(".gallery");
+
+
+const markup = galleryItems.reduce((acc, { preview, original, description }) =>
+	acc +
+	`
+ <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+   <img  class="gallery__image"
+    src="${preview}"
+     alt="${description}" 
+     data-source="${original}" loading="lazy" width="350">
+     </a>
+ </li>
+ `,
+	""
+);
+
+containerGallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
